114bf392efaf60c2dcdccc7544a96eb0944e0639
paulio11 <youngjpj@gmail.com>
2023-05-16 16:56:07 +0100

    Add routes for home and login to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Container } from "react-bootstrap";
 import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
+import LogIn from "./pages/auth/LogIn";
 
 function App() {
   return (
     <>
       <NavBar />
-      <h1>Hello</h1>
+      <Container>
+        <Routes>
+          <Route exact path="/" element={<h1>Home</h1>} />
+          <Route exact path="/login" element={<LogIn />} />
+        </Routes>
+      </Container>
     </>
   );
 }
