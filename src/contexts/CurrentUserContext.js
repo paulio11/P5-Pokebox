a9bdef2c651358a800798efab3c6e27479960a0a
paulio11 <youngjpj@gmail.com>
2023-05-16 15:57:55 +0100

    Add provider for managing current user state
    Create CurrentUserContext and SetCurrentUserContext to provide access to the current user and the function to set the current user.
    Implement custom hooks useCurrentUser and useSetCurrentUser for convenient access to the current user and the function to set the current user.
    Add CurrentUserProvider component to encapsulate the state management of the current user.
    Fetch the current user data from the server on component mount using an API request to dj-rest-auth/user/.
    Implement Axios interceptors to handle authentication tokens:
    Intercept requests using axiosReq and attempt to refresh the authentication token. If the token refresh fails, navigate to the login page.
    Intercept responses using axiosRes and handle token expiration errors by attempting to refresh the token. If the token refresh fails, navigate to the login page.
    Provide the current user and set current user functions to child components using the context providers.

---
+++ src/contexts/CurrentUserContext.js
@@ -0,0 +1,81 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import axios from "axios";
+import { axiosRes, axiosReq } from "../api/AxiosDefaults";
+import { useNavigate } from "react-router-dom";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+
+// Provider component to manage the current user state
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+
+  // Function to fetch the current user data on component mount
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  useMemo(() => {
+    // Intercept requests and attempt to refresh the authentication token
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              navigate("/login");
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+
+    // Intercept responses and handle token expiration errors
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                navigate("/login");
+              }
+              return null;
+            });
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  }, [navigate]);
+
+  // Provide the current user and set current user functions to child components
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+};
