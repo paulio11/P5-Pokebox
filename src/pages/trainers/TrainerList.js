bdd9b67b1155607f1f45cd98a40add73d72f9048
paulio11 <youngjpj@gmail.com>
2023-05-16 23:33:05 +0100

    Add user profile search
    Added search for to trainer list page. If a search query exists the API fetch request adds a parameter to the url.

--- src/pages/trainers/TrainerList.js
+++ src/pages/trainers/TrainerList.js
@@ -3,32 +3,56 @@ import LoadingText from "../../components/LoadingText";
 import { axiosReq } from "../../api/AxiosDefaults";
 import Trainer from "./Trainer";
 import styles from "../../styles/TrainerList.module.css";
+import { Form } from "react-bootstrap";
 
 const TrainerList = () => {
   const [loaded, setLoaded] = useState(false);
-  const [list, setList] = useState([]);
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axiosReq.get("profiles");
-        setList(response.data.results);
+        const response = await axiosReq.get(
+          query ? `profiles/?owner=${query}` : "profiles"
+        );
+        setResults(response.data.results);
         setLoaded(true);
       } catch (error) {}
     };
 
     setLoaded(false);
-    fetchData();
-  }, []);
+
+    const queryTimer = setTimeout(() => {
+      fetchData();
+    }, 1000);
+
+    return () => {
+      clearTimeout(queryTimer);
+    };
+  }, [query]);
+
+  const handleChange = (event) => {
+    setQuery(event.target.value.toLowerCase());
+    setLoaded(false);
+  };
 
   return (
     <>
       <h1>Our Pokémon Trainers</h1>
       <hr />
+      <Form className={styles.SearchForm}>
+        <Form.Label hidden>Search for a Trainer</Form.Label>
+        <Form.Control
+          value={query}
+          placeholder="Search"
+          onChange={handleChange}
+        />
+      </Form>
       {loaded ? (
         <>
-          <div className={styles.ListContainer}>
-            {list.map((trainer) => (
+          <div className={styles.ResultsContainer}>
+            {results.map((trainer) => (
               <Trainer key={trainer.id} {...trainer} />
             ))}
           </div>
