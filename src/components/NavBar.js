010ee62b0abd6bc8061c492d96bc3cccd552f921
paulio11 <youngjpj@gmail.com>
2023-05-16 15:50:08 +0100

    Style NavBar component
    Added NavBar.module.css to include my own styles for my navbar component. Downgraded react-bootstrap and bootstrap to get classNames mr-auto and ml-auto to function correctly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,37 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import pokeball from "../assets/pokeball.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <NavLink to="/">
+      <NavLink to="/" className={styles.NavBrand}>
         <Navbar.Brand className="d-flex align-items-center">
-          <img src={pokeball} alt="Pokébox Logo" />
+          <img src={pokeball} alt="Pokébox Logo" className={styles.BrandLogo} />
           <strong>POKÉBOX</strong>
         </Navbar.Brand>
       </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <NavLink to="/pokemon">Pokémon</NavLink>
-          <NavLink to="/trainers">Trainers</NavLink>
+          <NavLink to="/pokemon" className={styles.NavLink}>
+            Pokémon
+          </NavLink>
+          <NavLink to="/trainers" className={styles.NavLink}>
+            Trainers
+          </NavLink>
         </Nav>
         <Nav className="ml-auto">
-          <NavLink to="/login">Log in</NavLink>
-          <NavLink to="/login">Log out</NavLink>
-          <NavLink to="/login">Register</NavLink>
+          <NavLink to="/login" className={styles.NavLink}>
+            Log in
+          </NavLink>
+          <NavLink to="/login" className={styles.NavLink}>
+            Log out
+          </NavLink>
+          <NavLink to="/login" className={styles.NavLink}>
+            Register
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
