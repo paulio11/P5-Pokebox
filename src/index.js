f3fc8309fc1ecfd109a99a48699c5631aee90d14
paulio11 <youngjpj@gmail.com>
2023-05-16 16:56:37 +0100

    Import CurrentUserProvider and placed App inside

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
