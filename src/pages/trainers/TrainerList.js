69514308b739b57dee95774052610f1a3fe4b678
paulio11 <youngjpj@gmail.com>
2023-05-17 00:16:05 +0100

    Add profile list sorting
    Added sort form to trainer list page. Can sort by pokemon collection size, or username. Variables set my handleSortChange are passed into the api get request.

--- src/pages/trainers/TrainerList.js
+++ src/pages/trainers/TrainerList.js
@@ -9,12 +9,16 @@ const TrainerList = () => {
   const [loaded, setLoaded] = useState(false);
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState("");
+  const [sortBy, setSortBy] = useState("pokemon");
+  const [sortOrder, setSortOrder] = useState("desc");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axiosReq.get(
-          query ? `profiles/?owner=${query}` : "profiles"
+          query
+            ? `profiles/?owner=${query}`
+            : `profiles/?sort_by=${sortBy}&sort_order=${sortOrder}`
         );
         setResults(response.data.results);
         setLoaded(true);
@@ -30,25 +34,65 @@ const TrainerList = () => {
     return () => {
       clearTimeout(queryTimer);
     };
-  }, [query]);
+  }, [query, sortBy, sortOrder]);
 
   const handleChange = (event) => {
     setQuery(event.target.value.toLowerCase());
     setLoaded(false);
   };
 
+  const handleSortChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "sort_by") {
+      setSortBy(value);
+    } else if (name === "sort_order") {
+      setSortOrder(value);
+    }
+  };
+
   return (
     <>
       <h1>Our Pokémon Trainers</h1>
       <hr />
-      <Form className={styles.SearchForm}>
-        <Form.Label hidden>Search for a Trainer</Form.Label>
-        <Form.Control
-          value={query}
-          placeholder="Search"
-          onChange={handleChange}
-        />
+      <Form className={styles.SortForm}>
+        <Form.Group>
+          <Form.Label>Search</Form.Label>
+          <Form.Control
+            value={query}
+            placeholder="Search"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {!query && (
+          <>
+            <Form.Group>
+              <Form.Label>Sort By</Form.Label>
+              <Form.Control
+                as="select"
+                name="sort_by"
+                value={sortBy}
+                onChange={handleSortChange}
+              >
+                <option value="owner">Username</option>
+                <option value="pokemon">Pokémon collection</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Sort Order</Form.Label>
+              <Form.Control
+                as="select"
+                name="sort_order"
+                value={sortOrder}
+                onChange={handleSortChange}
+              >
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+              </Form.Control>
+            </Form.Group>
+          </>
+        )}
       </Form>
+
       {loaded ? (
         <>
           <div className={styles.ResultsContainer}>
