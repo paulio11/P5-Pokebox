867f6773f5d74e86408af45dd492293c9befd48f
paulio11 <youngjpj@gmail.com>
2023-05-16 17:50:48 +0100

    Style main container to add top padding

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Container } from "react-bootstrap";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import LogIn from "./pages/auth/LogIn";
+import Register from "./pages/auth/Register";
+import styles from "./styles/App.module.css";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Container>
+      <Container className={styles.MainContainer}>
         <Routes>
           <Route exact path="/" element={<h1>Home</h1>} />
           <Route exact path="/login" element={<LogIn />} />
+          <Route exact path="/register" element={<Register />} />
         </Routes>
       </Container>
     </>
