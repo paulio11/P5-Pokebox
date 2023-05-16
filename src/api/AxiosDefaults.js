b29f3b22542c85b9234bf8280701033568021839
paulio11 <youngjpj@gmail.com>
2023-05-16 19:33:28 +0100

    Fix issue with pokemon list on refresh
    There was an issue with the api requests on a page reload. Names would fetch but data would not. Adding the cache control headers to the axios defaults fixed this issue.

--- src/api/AxiosDefaults.js
+++ src/api/AxiosDefaults.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export const pokeApi = axios.create({
   baseURL: "https://pokeapi.co/api/v2/",
   withCredentials: false,
+  // To fix issues on page reload
+  headers: { "Cache-Control": "no-cache" },
 });
 
 axios.defaults.baseURL = "https://project-5-backend.herokuapp.com/";
