0e77603968eeae9dca65fb3a471a565b3ccef2fc
paulio11 <youngjpj@gmail.com>
2023-05-17 16:51:18 +0100

    Add links to trainer profiles

--- src/pages/trainers/Trainer.js
+++ src/pages/trainers/Trainer.js
@@ -1,47 +1,51 @@
 import React from "react";
 import styles from "../../styles/Trainer.module.css";
 import { ProgressBar, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Trainer = (props) => {
   const { id, owner, avatar, pokemon, about, created } = props;
+
   return (
     <>
       <div className={styles.TrainerCard}>
-        <Row>
-          <Col className={styles.Header}>
-            <span>ID: #{id}</span>
-            <span>{created}</span>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={3}>
-            <img
-              src={avatar}
-              alt={`${owner}'s avatar`}
-              className={styles.Avatar}
-            />
-          </Col>
-          <Col>
-            <div className={styles.TrainerName}>{owner}</div>
-            <div>{about}</div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <hr />
-            <div className={styles.Footer}>
-              <ProgressBar
-                now={pokemon.length}
-                max={1010}
-                variant="danger"
-                className={styles.CollectionBar}
+        <Link className={styles.TrainerLink} to={`/trainer/${id}`}>
+          <Row>
+            <Col className={styles.Header}>
+              <span>ID: #{id}</span>
+              <span>{created}</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={3}>
+              <img
+                src={avatar}
+                alt={`${owner}'s avatar`}
+                className={styles.Avatar}
               />
-              <div className={styles.CollectionPercent}>
-                {((pokemon.length / 1010) * 100).toFixed(2)}%
+            </Col>
+            <Col>
+              <div className={styles.TrainerName}>{owner}</div>
+              <div>{about}</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <hr />
+              <div className={styles.Footer}>
+                <ProgressBar
+                  now={pokemon.length}
+                  max={1010}
+                  variant="danger"
+                  className={styles.CollectionBar}
+                />
+                <div className={styles.CollectionPercent}>
+                  {((pokemon.length / 1010) * 100).toFixed(2)}%
+                </div>
               </div>
-            </div>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </Link>
       </div>
     </>
   );
