864d352d1d17a8457a80b8538a9ffa78cc661000
paulio11 <youngjpj@gmail.com>
2023-05-18 19:33:59 +0100

    Add final post component styles

--- src/pages/dairy/Post.js
+++ src/pages/dairy/Post.js
@@ -1,25 +1,43 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { axiosRes } from "../../api/AxiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
 
 const Post = (props) => {
-  const { id, owner, created, body, image, like_id, like_count, setPosts } =
-    props;
+  const {
+    id,
+    owner,
+    created,
+    body,
+    image,
+    like_id,
+    like_count,
+    profile_id,
+    profile_avatar,
+    setPosts,
+  } = props;
+  const currentUser = useCurrentUser();
 
   const handleLike = async () => {
-    try {
-      const { data } = await axiosRes.post("/likes/", { post: id });
-      setPosts((prevPosts) => ({
-        ...prevPosts,
-        results: prevPosts.results.map((post) => {
-          return post.id === id
-            ? { ...post, like_count: post.like_count + 1, like_id: data.id }
-            : post;
-        }),
-      }));
-    } catch (error) {}
+    if (currentUser?.username === owner || !currentUser) {
+      return;
+    } else {
+      try {
+        const { data } = await axiosRes.post("/likes/", { post: id });
+        setPosts((prevPosts) => ({
+          ...prevPosts,
+          results: prevPosts.results.map((post) => {
+            return post.id === id
+              ? { ...post, like_count: post.like_count + 1, like_id: data.id }
+              : post;
+          }),
+        }));
+      } catch (error) {}
+    }
   };
+
   const handleUnlike = async () => {
     try {
       await axiosRes.delete(`likes/${like_id}`);
@@ -38,21 +56,49 @@ const Post = (props) => {
     <>
       <div className={styles.Post}>
         <div className={styles.Header}>
-          <span className={styles.Created}>{created}</span>
-          {like_id ? (
-            <button onClick={handleUnlike}>Unlike</button>
-          ) : (
-            <button onClick={handleLike}>Like</button>
-          )}
-          <div>
-            <span>{like_count}</span>
+          <Link to={`/post/${id}`} className={styles.Link}>
+            {created}
+          </Link>
+          <div className={styles.LikeContainer}>
+            <span className={styles.LikeCount}>{like_count}</span>
+            <OverlayTrigger
+              overlay={
+                <Tooltip>
+                  {!currentUser && "Log in to like this post."}
+                  {currentUser?.username === owner &&
+                    "You can't like your own post."}
+                </Tooltip>
+              }
+            >
+              {like_id ? (
+                <i
+                  className={`fas fa-heart ${styles.LikeHeart} ${styles.Liked}`}
+                  onClick={handleUnlike}
+                />
+              ) : (
+                <i
+                  className={`far fa-heart ${styles.LikeHeart}`}
+                  onClick={handleLike}
+                />
+              )}
+            </OverlayTrigger>
           </div>
         </div>
         <hr />
         <Row>
           <Col xs={12} lg={image ? 6 : undefined}>
             <div className={styles.Body}>{body}</div>
-            <div className={styles.Owner}>- {owner}</div>
+            <Link
+              to={`/trainer/${profile_id}`}
+              className={`${styles.Link} ${styles.Owner}`}
+            >
+              {owner}
+              <img
+                src={profile_avatar}
+                alt={`${owner}'s avatar`}
+                className={styles.Avatar}
+              />
+            </Link>
           </Col>
           {image && (
             <Col xs={12} lg={6}>
