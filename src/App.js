6f560d37593a10dffe748165da287a5f9f48b3dc
paulio11 <youngjpj@gmail.com>
2023-05-30 21:56:42 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -12,26 +12,36 @@ import PostPage from "./pages/dairy/PostPage";
 import PostForm from "./pages/dairy/PostForm";
 import PostEditForm from "./pages/dairy/PostEditForm";
 import ToTopButton from "./components/ToTopButton";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div className={styles.Page}>
+        <div>
+          <NavBar />
+          <Container className={styles.MainContainer}>
+            <Routes>
+              <Route exact path="/" element={<h1>Home</h1>} />
+              <Route exact path="/login" element={<LogIn />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/pokemon" element={<PokemonList />} />
+              <Route exact path="/pokemon/:id" element={<PokemonDexPage />} />
+              <Route exact path="/trainers" element={<TrainerList />} />
+              <Route
+                exact
+                path="/trainer/:id"
+                element={<TrainerProfilePage />}
+              />
+              <Route exact path="/post/:id" element={<PostPage />} />
+              <Route exact path="/newpost" element={<PostForm />} />
+              <Route exact path="/editpost/:id" element={<PostEditForm />} />
+            </Routes>
+          </Container>
+        </div>
+        <Footer />
+      </div>
       <ToTopButton />
-      <NavBar />
-      <Container className={styles.MainContainer}>
-        <Routes>
-          <Route exact path="/" element={<h1>Home</h1>} />
-          <Route exact path="/login" element={<LogIn />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/pokemon" element={<PokemonList />} />
-          <Route exact path="/pokemon/:id" element={<PokemonDexPage />} />
-          <Route exact path="/trainers" element={<TrainerList />} />
-          <Route exact path="/trainer/:id" element={<TrainerProfilePage />} />
-          <Route exact path="/post/:id" element={<PostPage />} />
-          <Route exact path="/newpost" element={<PostForm />} />
-          <Route exact path="/editpost/:id" element={<PostEditForm />} />
-        </Routes>
-      </Container>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return <div className={styles.Footer}>Pokébox by Paul Young</div>;
+};
+
+export default Footer;
